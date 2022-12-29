52950c420a1cf5981c2f0e448b59a990d358bdff
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-12-29 20:58:50 -0300

    feat: render users list

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const response = await axios.get('http://localhost:3000/users')
+  await pause(2000)
+  return response.data
+})
+
+const pause = (duration) => {
+  return new Promise((resolve) => setTimeout(resolve, duration))
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    keyframes: {
+      shimmer: {
+        '100%': { transform: 'translateX(100%)' },
+      },
+    },
+    animation: {
+      shimmer: 'shimmer 1.5s infinite',
+    },
   },
   plugins: [],
 }
